b0d87676c15736c138f495c2b376053754db8351
ledungdee <feelgoood98@gmail.com>
2022-12-11 12:38:07 +0700

    edit page title,css admin page,edit currency"

--- bookstore/index.php
+++ bookstore/index.php
@@ -2,6 +2,7 @@
 <meta http-equiv="Content-Type"'.' content="text/html; charset=utf8"/>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="style.css">
+<title>Book Store</title>
 <body>
 <?php
 session_start();
@@ -93,7 +94,7 @@ echo '<blockquote>';
     while($row = $result->fetch_assoc()) {
 	    echo "<td>";
 	    echo "<table>";
-	   	echo '<tr><td>'.'<img src="'.$row["Image"].'"width="80%">'.'</td></tr><tr><td style="padding: 5px;">Title: '.$row["BookTitle"].'</td></tr><tr><td style="padding: 5px;">ISBN: '.$row["ISBN"].'</td></tr><tr><td style="padding: 5px;">Author: '.$row["Author"].'</td></tr><tr><td style="padding: 5px;">Type: '.$row["Type"].'</td></tr><tr><td style="padding: 5px;">RM'.$row["Price"].'</td></tr><tr><td style="padding: 5px;">
+	   	echo '<tr><td>'.'<img src="'.$row["Image"].'"width="80%">'.'</td></tr><tr><td style="padding: 5px;">Title: '.$row["BookTitle"].'</td></tr><tr><td style="padding: 5px;">ISBN: '.$row["ISBN"].'</td></tr><tr><td style="padding: 5px;">Author: '.$row["Author"].'</td></tr><tr><td style="padding: 5px;">Type: '.$row["Type"].'</td></tr><tr><td style="padding: 5px;">$'.$row["Price"].'</td></tr><tr><td style="padding: 5px;">
 	   	<form action="" method="post">
 	   	Quantity: <input type="number" value="1" name="quantity" style="width: 20%"/><br>
 	   	<input type="hidden" value="'.$row['BookID'].'" name="ac"/>
@@ -114,13 +115,13 @@ echo '<blockquote>';
     while($row = $result->fetch_assoc()){
     	echo "<tr><td>";
     	echo '<img src="'.$row["Image"].'"width="20%"><br>';
-    	echo $row['BookTitle']."<br>RM".$row['Price']."<br>";
+    	echo $row['BookTitle']."<br>$".$row['Price']."<br>";
     	echo "Quantity: ".$row['Quantity']."<br>";
-    	echo "Total Price: RM".$row['TotalPrice']."</td></tr>";
+    	echo "Total Price: $".$row['TotalPrice']."</td></tr>";
     	$total += $row['TotalPrice'];
     }
     echo "<tr><td style='text-align: right;background-color: #f2f2f2;''>";
-    echo "Total: <b>RM".$total."</b><center><form action='checkout.php' method='post'><input class='button' type='submit' name='checkout' value='CHECKOUT'></form></center>";
+    echo "Total: <b>$".$total."</b><center><form action='checkout.php' method='post'><input class='button' type='submit' name='checkout' value='CHECKOUT'></form></center>";
     echo "</td></tr>";
 	echo "</table>";
 	echo '</blockquote>';
