029d6fa2ecf68804bcaccc55673b7509c322275b
ledungdee <feelgoood98@gmail.com>
2022-12-11 16:27:07 +0700

    Del unused image, readme,..."

--- admin/partials/menu.php
+++ admin/partials/menu.php
@@ -15,11 +15,10 @@
         <div class="menu text-center">
             <div class="wrapper">
                 <ul>
-                    <li><a href="index.php">Home</a></li>
+                    <li><a href="index.php">Manager User</a></li>
                     <li><a href="manage-admin.php">Admin</a></li>
-                    <li><a href="manage-category.php">Category</a></li>
                     <li><a href="manage-book.php">Books</a></li>
-                    <li><a href="manage-order.php">Order</a></li>
+                    <li><a href="manage-order.php">Order Information</a></li>
                     <li><a href="logout.php">LogOut</a></li>
                 </ul>
             </div>   
